b35b4314aa9961b312ad02c54c574c5c2bc5bc9c
Harsh Thakur <harshthakur45n@gmail.com>
2026-05-19 13:32:02 +0530

    change footer color

--- FrontendDevOps/src/components/Footer/index.jsx
+++ FrontendDevOps/src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="py-3 bg-black text-white/80 font-semibold">
+    <footer className="py-3 bg-red text-white/80 font-semibold">
       <p className="text-center hover:text-purple-500 transition-colors">
         &copy; 2026 CareerConnect. All rights reserved.
       </p>
